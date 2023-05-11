5ea242b6fa1abbb47dd8917227e6e28ef8e6e747
chimchimster <jorddyishere@gmail.com>
2023-05-11 16:24:24 +0600

    247 page

--- online_store/myshop/orders/admin.py
+++ online_store/myshop/orders/admin.py
@@ -1,5 +1,28 @@
+import csv
+import datetime
+from django.http import HttpResponse
 from django.contrib import admin
 from .models import Order, OrderItem
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment;filename={opts.verbose_name}.csv'
+    writer = csv.writer(response)
+
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    writer.writerow([field.verbose_name for field in fields])
+
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+        export_to_csv.short_description = 'Export to CSV'
+    return response
 
 
 class OrderItemInLine(admin.TabularInline):
@@ -12,3 +35,5 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated',)
     list_filter = ('paid', 'created', 'updated',)
     inlines = (OrderItemInLine,)
+    actions = (export_to_csv,)
+
